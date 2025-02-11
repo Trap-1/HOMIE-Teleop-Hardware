2f510b94630214811d2620ca77c05abe8ef0e232
Feiyu Jia <feiyujia635@gmail.com>
2025-02-11 21:39:47 +0800

    release

---
+++ Keil code/Foot_Pedal_keil_code/OBJ/mpu6050.d
@@ -0,0 +1,15 @@
+..\obj\mpu6050.o: ..\HARDWARE\MUP6050\MPU6050.c
+..\obj\mpu6050.o: ..\USER\stm32f10x.h
+..\obj\mpu6050.o: ..\CORE\core_cm3.h
+..\obj\mpu6050.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\mpu6050.o: ..\USER\system_stm32f10x.h
+..\obj\mpu6050.o: ..\USER\stm32f10x_conf.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\stm32f10x_adc.h
+..\obj\mpu6050.o: ..\USER\stm32f10x.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\stm32f10x_dbgmcu.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\stm32f10x_gpio.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\stm32f10x_rcc.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\stm32f10x_usart.h
+..\obj\mpu6050.o: ..\STM32F10x_FWLib\inc\misc.h
+..\obj\mpu6050.o: ..\HARDWARE\MyI2C\MyI2C.h
+..\obj\mpu6050.o: ..\HARDWARE\MUP6050\MPU6050_Reg.h
